80d60b6d24049e794677f928c9d67009ba42f648
Dominique Unruh <unruh@ut.ee>
2016-04-03 12:08:39 +0300

    Moved mathquill-source out of html/ directory

--- haskell/gui/html/misc.js
+++ haskell/gui/html/misc.js
@@ -1,4 +1,7 @@
 function m_to_pmml() {
+    console.log("alive 1");
+    blubb();
+    console.log("alive 2");
     pmml = m.pmathml();
     pmml2 = $("<tmp/>").append(pmml).html();
     //console.log("m_to_pmml",pmml,pmml2);
