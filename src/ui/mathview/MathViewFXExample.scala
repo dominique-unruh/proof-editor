8bf0be946acebb21af174dbbb2b5f705a99f3ad2
Dominique Unruh <dominique@unruh.de>
2016-08-01 14:33:22 +0300

    minimizing

--- src/ui/mathview/MathViewFXExample.scala
+++ src/ui/mathview/MathViewFXExample.scala
@@ -3,10 +3,10 @@ package ui.mathview
 import java.lang.Double
 import java.util.concurrent.Callable
 import javafx.application.Application
+import javafx.scene.Group
 import javafx.scene.layout.{HBox, VBox}
 import javafx.scene.shape.Line
 import javafx.scene.text.Text
-import javafx.scene.{Group, Node}
 import javafx.stage.Stage
 
 import scalafx.Includes._
@@ -18,20 +18,21 @@ class MathViewFXExample extends Application {
     val nh2 = new Text("x")
     val nbinopc = new HBox(nh2)
     val nbinop = new Group(nbinopc)
-//    nbinop.getChildren.setAll(nbinopc)
 
+    val line = new Line()
+    val innerWidth = javafx.beans.binding.Bindings.createDoubleBinding(
+      new Callable[Double] {
+        override def call(): Double = nbinop.layoutBounds.get.getWidth
+      },
+      nbinop.layoutBounds)
+    line.endX <== innerWidth
 
-
-    val nw = new Group()
-    val nwc = new Fraction(nbinop)
+    val nwc = new VBox(line, nbinop)
     nwc.boundsInLocalProperty().onChange({})
-    nw.getChildren.setAll(nwc)
 
-    new HBox(nw)
+    val nw = new Group(nwc)
     nw.layoutBoundsProperty().onChange({})
-//    new BinOp(nw)
 
-//    val parent = nbinopc
     nbinopc.getChildren.remove(nh2)
 
     sys.exit()
@@ -42,18 +43,4 @@ object MathViewFXExample {
   def main(args: Array[String]): Unit = {
     Application.launch(classOf[MathViewFXExample])
   }
-}
-
-class Fraction(b:Node) extends VBox {
-  val line = new Line()
-  getChildren.addAll(line, b)
-
-  val innerWidth = javafx.beans.binding.Bindings.createDoubleBinding(
-    new Callable[Double] {
-      override def call(): Double = b.layoutBounds.get.getWidth
-    },
-    b.layoutBounds)
-
-  line.startX = 0
-  line.endX <== innerWidth
 }
